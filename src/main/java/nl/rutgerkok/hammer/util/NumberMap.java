e4a551dbf6cf8b12aa4a2305578f97ca1c746063
rutgerkok <rutger.kok@live.com>
2015-07-21 21:04:25 +0200

    Add draft of NumberMap, will become useful to convert ids

---
+++ src/main/java/nl/rutgerkok/hammer/util/NumberMap.java
@@ -0,0 +1,63 @@
+package nl.rutgerkok.hammer.util;
+
+/**
+ * Simple class that maps numbers (represented by chars, as those are
+ * essentially unsigned shorts) to each other.
+ *
+ */
+public final class NumberMap {
+
+    private char[] idMapping = new char[0];
+
+    public void addId(char originalId, char translatedId) {
+        // Mapping to 0 (null) is special cased
+        if (translatedId == 0) {
+            if (originalId != 0) {
+                throw new IllegalArgumentException("Can't map to translated id of 0 (null) unless the original id is also 0");
+            }
+            return;
+        }
+
+        if (originalId >= idMapping.length) {
+            growToSize(originalId);
+        }
+
+        idMapping[originalId] = translatedId;
+    }
+
+    public char getTranslatedId(char originalId) throws MaterialNotFoundException {
+
+        if (originalId == 0) {
+            return 0;
+        }
+
+        if (originalId > idMapping.length) {
+            throw new MaterialNotFoundException(originalId);
+        }
+
+        char translatedId = idMapping[originalId];
+        if (translatedId == 0) {
+            // No mapping found
+            throw new MaterialNotFoundException(originalId);
+        }
+
+        return translatedId;
+    }
+
+    private void growToSize(int highestIndexThatMustFit) {
+        // Need to resize array
+        int newSize = Math.max(1, idMapping.length);
+        while (newSize <= highestIndexThatMustFit) {
+            newSize *= 2;
+
+            if (newSize <= 0 || newSize > Character.MAX_VALUE * 2) {
+                // This should be impossible, as newSize only grows in powers of
+                // two until it can fit a char
+                throw new AssertionError("newSize got too big: " + newSize);
+            }
+        }
+        char[] newArray = new char[newSize];
+        System.arraycopy(idMapping, 0, newArray, 0, idMapping.length);
+        idMapping = newArray;
+    }
+}
