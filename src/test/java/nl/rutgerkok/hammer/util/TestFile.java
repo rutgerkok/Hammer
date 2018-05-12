6adae0cdc34d514ec81ec30a3474f7cadb9def52
Rutger Kok <rutger.kok@live.com>
2018-05-12 14:36:13 +0200

    Simplify this method

--- src/test/java/nl/rutgerkok/hammer/util/TestFile.java
+++ src/test/java/nl/rutgerkok/hammer/util/TestFile.java
@@ -1,6 +1,5 @@
 package nl.rutgerkok.hammer.util;
 
-import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -14,10 +13,6 @@ public final class TestFile {
      * @return The file.
      */
     public static final Path get(String name) {
-        try {
-            return Paths.get(TestFile.class.getResource("/" + name).toURI());
-        } catch (URISyntaxException e) {
-            throw new AssertionError("Failed to get file for " + name, e);
-        }
+        return Paths.get("src/test/resources/" + name);
     }
 }
