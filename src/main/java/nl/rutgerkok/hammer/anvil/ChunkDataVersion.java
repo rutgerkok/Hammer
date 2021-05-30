90f1a9103eb2a21936b80756acedea6d0b9e9dbb
Rutger Kok <rutger.kok@live.com>
2021-05-30 15:39:49 +0200

    Add a bunch of data versions

--- src/main/java/nl/rutgerkok/hammer/anvil/ChunkDataVersion.java
+++ src/main/java/nl/rutgerkok/hammer/anvil/ChunkDataVersion.java
@@ -6,7 +6,38 @@ package nl.rutgerkok.hammer.anvil;
  */
 public final class ChunkDataVersion implements Comparable<ChunkDataVersion> {
 
+    public static final ChunkDataVersion MINECRAFT_1_9 = new ChunkDataVersion(169);
+    public static final ChunkDataVersion MINECRAFT_1_9_1 = new ChunkDataVersion(175);
+    public static final ChunkDataVersion MINECRAFT_1_9_2 = new ChunkDataVersion(176);
+    public static final ChunkDataVersion MINECRAFT_1_9_3 = new ChunkDataVersion(183);
+    public static final ChunkDataVersion MINECRAFT_1_9_4 = new ChunkDataVersion(184);
+    public static final ChunkDataVersion MINECRAFT_1_10 = new ChunkDataVersion(510);
+    public static final ChunkDataVersion MINECRAFT_1_10_1 = new ChunkDataVersion(511);
+    public static final ChunkDataVersion MINECRAFT_1_10_2 = new ChunkDataVersion(512);
+    public static final ChunkDataVersion MINECRAFT_1_11 = new ChunkDataVersion(819);
+    public static final ChunkDataVersion MINECRAFT_1_11_1 = new ChunkDataVersion(921);
+    public static final ChunkDataVersion MINECRAFT_1_11_2 = new ChunkDataVersion(922);
+    public static final ChunkDataVersion MINECRAFT_1_12 = new ChunkDataVersion(1139);
+    public static final ChunkDataVersion MINECRAFT_1_12_1 = new ChunkDataVersion(1241);
     public static final ChunkDataVersion MINECRAFT_1_12_2 = new ChunkDataVersion(1343);
+    public static final ChunkDataVersion MINECRAFT_1_13 = new ChunkDataVersion(1519);
+    public static final ChunkDataVersion MINECRAFT_1_13_1 = new ChunkDataVersion(1628);
+    public static final ChunkDataVersion MINECRAFT_1_13_2 = new ChunkDataVersion(1631);
+    public static final ChunkDataVersion MINECRAFT_1_14 = new ChunkDataVersion(1952);
+    public static final ChunkDataVersion MINECRAFT_1_14_1 = new ChunkDataVersion(1957);
+    public static final ChunkDataVersion MINECRAFT_1_14_2 = new ChunkDataVersion(1963);
+    public static final ChunkDataVersion MINECRAFT_1_14_3 = new ChunkDataVersion(1968);
+    public static final ChunkDataVersion MINECRAFT_1_14_4 = new ChunkDataVersion(1976);
+    public static final ChunkDataVersion MINECRAFT_1_15 = new ChunkDataVersion(2225);
+    public static final ChunkDataVersion MINECRAFT_1_15_1 = new ChunkDataVersion(2227);
+    public static final ChunkDataVersion MINECRAFT_1_15_2 = new ChunkDataVersion(2230);
+    public static final ChunkDataVersion MINECRAFT_1_16 = new ChunkDataVersion(2566);
+    public static final ChunkDataVersion MINECRAFT_1_16_1 = new ChunkDataVersion(2567);
+    public static final ChunkDataVersion MINECRAFT_1_16_2 = new ChunkDataVersion(2578);
+    public static final ChunkDataVersion MINECRAFT_1_16_3 = new ChunkDataVersion(2580);
+    public static final ChunkDataVersion MINECRAFT_1_16_4 = new ChunkDataVersion(2584);
+    public static final ChunkDataVersion MINECRAFT_1_16_5 = new ChunkDataVersion(2586);
+
 
     /**
      * Gets the chunk data version from the version stored in the chunk root
@@ -28,7 +59,7 @@ public final class ChunkDataVersion implements Comparable<ChunkDataVersion> {
      * @return The format.
      */
     public static ChunkDataVersion latest() {
-        return new ChunkDataVersion(1493); // Snapshot 18w20c
+        return new ChunkDataVersion(2716); // 1.17 pre-release 1
     }
 
     private final int id;
